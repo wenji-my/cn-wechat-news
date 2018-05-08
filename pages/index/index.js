913f2068222f3c83c67aa42a0a379dca92855882
wenji-my <my841686869@163.com>
2018-05-08 14:56:13 +0800

    restructure index page

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,49 +1,47 @@
-const newsType = [
+let newsType = [
   {
     type: "gn",
-    name: "国内"
+    name: "国内",
+    showLoading: true
   },
   {
     type: "gj",
-    name: "国际"
+    name: "国际",
+    showLoading: true
   },
   {
     type: "cj",
-    name: "财经"
+    name: "财经",
+    showLoading: true
   },
   {
     type: "yl",
-    name: "娱乐"
+    name: "娱乐",
+    showLoading: true
   },
   {
     type: "js",
-    name: "军事"
+    name: "军事",
+    showLoading: true
   },
   {
     type: "ty",
-    name: "体育"
+    name: "体育",
+    showLoading: true
   },
   {
     type: "other",
-    name: "其他"
+    name: "其他",
+    showLoading: true
   }
 ]
 Page({
   data:{
     isIphoneX: false,
     newsType: newsType,
-    clientHeight: 0,
     currentTab: 0,
-    cacheNews: [],
     currentNews: [],
-    tabClickedMap: {},
-    gnNews: [],
-    gjNews: [],
-    cjNews: [],
-    ylNews: [],
-    jsNews: [],
-    tyNews: [],
-    otherNews: []
+    preCurrent: 0
   },
 
   // onPullDownRefresh(){
@@ -51,8 +49,7 @@ Page({
   // },
 
   onLoad() {
-    this.initAllNews()
-    //高度自适应
+    //适配iPhoneX
     wx.getSystemInfo({
       success: res => {
         let model = res.model;
@@ -61,9 +58,22 @@ Page({
             isIphoneX: true
           })
         }
-        let clientHeight = res.windowHeight
+      }
+    })
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: 'gn'
+      },
+      success: res => {
+        let result = res.data.result
+        // console.log(result)
+        this.data.newsType[0].showLoading = false
+        let newsType = this.data.newsType
+        // console.log(this.data.newsLoading)
         this.setData({
-          clientHeight: clientHeight
+          currentNews: result,
+          newsType: newsType
         })
       }
     })
@@ -73,10 +83,34 @@ Page({
   //   console.log(1)
   // },
 
-  scrollTab(e){
+  scrollTab(e) {
     let current = e.detail.current
     this.setData({
       currentTab: current
+    })
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: newsType[current].type
+      },
+      success: res => {
+        let result = res.data.result
+        // console.log(result)
+        this.data.newsType[current].showLoading = false
+        this.data.newsType[this.data.preCurrent].showLoading = true
+        let newsType = this.data.newsType
+        // console.log(this.data.newsLoading)
+        this.setData({
+          currentNews: result,
+          // newsType: newsType,
+          preCurrent: current
+        })
+        setTimeout(() => {
+          this.setData({
+            newsType: newsType
+          })
+        }, 10)
+      }
     })
   },
 
@@ -89,93 +123,6 @@ Page({
         currentTab: index
       })
     }
-  },
-
-  onTapNews(e){
-    let type = e.currentTarget.dataset.type
-    let id = 0
-    switch (type) {
-      case 'gn':
-        id = this.data.gnNews[e.currentTarget.dataset.index].id
-        break;
-      case 'gj':
-        id = this.data.gjNews[e.currentTarget.dataset.index].id
-        break;
-      case 'cj':
-        id = this.data.cjNews[e.currentTarget.dataset.index].id
-        break;
-      case 'yl':
-        id = this.data.ylNews[e.currentTarget.dataset.index].id
-        break;
-      case 'js':
-        id = this.data.jsNews[e.currentTarget.dataset.index].id
-        break;
-      case 'ty':
-        id = this.data.tyNews[e.currentTarget.dataset.index].id
-        break;
-      case 'other':
-        id = this.data.otherNews[e.currentTarget.dataset.index].id
-        break;
-    }
-    wx.navigateTo({
-      url: '/pages/detail/detail?id=' + id
-    })
-  },
-
-  initAllNews(){
-    newsType.forEach(data => {
-      wx.request({
-        url: 'https://test-miniprogram.com/api/news/list',
-        data: {
-          type: data.type
-        },
-        header: {
-          'content-type': 'application/json' // 默认值
-        },
-        success: res => {
-          if (res.data.code === 200) {
-            let result = res.data.result
-            switch (data.type) {
-              case 'gn':
-                this.setData({
-                  gnNews: result
-                })
-                break;
-              case 'gj':
-                this.setData({
-                  gjNews: result
-                })
-                break;
-              case 'cj':
-                this.setData({
-                  cjNews: result
-                })
-                break;
-              case 'yl':
-                this.setData({
-                  ylNews: result
-                })
-                break;
-              case 'js':
-                this.setData({
-                  jsNews: result
-                })
-                break;
-              case 'ty':
-                this.setData({
-                  tyNews: result
-                })
-                break;
-              case 'other':
-                this.setData({
-                  otherNews: result
-                })
-                break;
-            }
-          }
-        }
-      })
-    })
   }
 
 })
